cdf9d500cb65f71a82b6b44f48390761753e263e
d11wtq <chris@w3style.co.uk>
2015-07-19 11:10:01 +0000

    Initial commit (basic wiring spike).

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express')
+  , bodyParser = require('body-parser')
+  , YAML = require('yamljs')
+  , deploy = require('./lib/aws/cloudformation/deploy')
+  ;
+
+var app = express();
+app.use(bodyParser.text({type: 'text/yaml'}));
+
+app.post('/deploy/:name', function(req, res){
+  if (req.headers['content-type'] == 'text/yaml') {
+    var deployment = deploy(
+      req.params.name,
+      YAML.parse(req.body)
+    );
+
+    res.writeHead(202, {'Content-Type': 'text/plain'});
+
+    deployment.on('log', function(msg){
+      res.write(msg);
+    });
+
+    deployment.on('end', function(){
+      res.end();
+    });
+  } else {
+    res.status(400).send('Unsupported content-type');
+  }
+});
+
+var server = app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
