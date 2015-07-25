61de22637539543cc0d1d270077bde1925737d74
d11wtq <chris@w3style.co.uk>
2015-07-25 09:20:32 +0000

    Implement Stack deletion endpoint.

--- app.js
+++ app.js
@@ -2,40 +2,53 @@ var express = require('express')
   , bodyParser = require('body-parser')
   , YAML = require('yamljs')
   , deploy = require('./lib/aws/cloudformation/deploy')
+  , remove = require('./lib/aws/cloudformation/remove')
   ;
 
-var app = express()
-  , timeout = 7200000
-  ;
+var app = express();
 
+/*! Handle YAML in request bodies */
 app.use(bodyParser.text({type: 'text/yaml'}));
 
-app.post('/deploy/:name', function(req, res){
-  req.setTimeout(timeout);
+/**
+ * Create or update :stack with the posted deploy.yml.
+ */
+app.put('/deployments/:stack', function(req, res){
+  req.setTimeout(7200000);
 
   if (req.headers['content-type'] == 'text/yaml') {
-    var deployment = deploy(
-      req.params.name,
-      YAML.parse(req.body)
-    );
+    var deployment = deploy(req.params.stack, YAML.parse(req.body));
 
-    res.writeHead(202, {'Content-Type': 'text/plain'});
-
-    deployment.on('log', function(msg){
-      res.write(msg);
-    });
-
-    deployment.on('end', function(){
-      res.end();
-    });
+    res.status(202).type('plain');
+    deployment.on('log', res.write.bind(res));
+    deployment.on('end', res.end.bind(res));
   } else {
     res.status(400).send('Unsupported content-type');
   }
 });
 
-var server = app.listen(3000, function(){
-  var host = server.address().address;
-  var port = server.address().port;
+/**
+ * Delete :stack, if it exists.
+ */
+app.delete('/deployments/:stack', function(req, res){
+  req.setTimeout(7200000);
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  var removal = remove(req.params.stack);
+
+  res.status(202).type('plain');
+  removal.on('log', res.write.bind(res));
+  removal.on('end', res.end.bind(res));
+});
+
+/*! Run express server */
+var server = app.listen(3000, function(){
+  var host = server.address().address
+    , port = server.address().port
+    ;
+
+  console.log(
+    'PaaS listening on http://%s:%s',
+    host,
+    port
+  );
 });
